d7acd4b1358d2683cda8e04beb7423114ca1c65e
chapmanb <chapmanb@50mail.com>
2015-01-16 11:28:38 -0500

    Support sambamba 0.5.1 indexing without ability to specify output bai file location

--- src/bcbio/align/bam.clj
+++ src/bcbio/align/bam.clj
@@ -1,15 +1,22 @@
 (ns bcbio.align.bam
   "Manipulate BAM files, using the Picard samtools API"
   (:import [htsjdk.samtools SAMFileReader SAMFileReader$ValidationStringency])
-  (:require [bcbio.run.itx :as itx]
-            [clojure.java.io :as io]))
+  (:require [bcbio.run.fsp :as fsp]
+            [bcbio.run.itx :as itx]
+            [clojure.core.strint :refer [<<]]
+            [clojure.java.io :as io]
+            [me.raynes.fs :as fs]))
 
 (defn do-index
   "Index BAM file"
   [bam-file]
   (let [out-file (str bam-file ".bai")]
-    (itx/run-cmd out-file
-                 "sambamba index ~{bam-file} ~{out-file}")))
+    (when (or (itx/needs-run? out-file) (not (itx/up-to-date? out-file bam-file)))
+      (itx/with-tx-file [tx-out-file out-file]
+        (let [tx-bam-file (fsp/file-root tx-out-file)
+              full-bam-file (str (fs/file bam-file))]
+          (itx/check-run (<< "ln -s ~{full-bam-file} ~{tx-bam-file}"))
+          (itx/check-run (<< "sambamba index ~{tx-bam-file}")))))))
 
 (defn sample-names
   "Retrieve samples represented in the BAM file."
